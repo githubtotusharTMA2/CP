6d724e7f1f4dffc55ef846150d99d7066587ad9c
Tushar Singh <tushars@adobe.com>
2024-04-17 19:59:25 +0530

    updated

---
+++ simpleCodechef.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int testcases = sc.nextInt();
+	    StringBuilder sb = new StringBuilder();
+	    
+	    while(testcases-- > 0) {
+            int n = sc.nextInt();
+
+            sb.append("\n");
+	    }
+	    
+	    System.out.println(sb);
+	}
+}
